f2f5d9e91e13fafb1243993aac3ff64622b0c2d8
leoCaversan <leocaversan7@gmail.com>
2023-05-19 22:35:59 -0300

    arquivos ajustados e tela de usuarios ajustadas com conexao com json, simulando banco de dados

--- clone-netflix/src/app/components/perfil-browse/perfil-browse.component.ts
+++ clone-netflix/src/app/components/perfil-browse/perfil-browse.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import profiles from '../../../assets/data/profile.json'
+
+
+interface Profile{
+  id:number,
+  name:string,
+  img:string  
+}
 
 @Component({
   selector: 'app-perfil-browse',
   templateUrl: './perfil-browse.component.html',
   styleUrls: ['./perfil-browse.component.css']
 })
-export class PerfilBrowseComponent {
+export class PerfilBrowseComponent implements OnInit {
+
+  profilesAvatar:Profile[] = profiles;
+  
+  constructor(){ }
+
+  ngOnInit(): void {
+  
+  }
+  
+  
+
 
 }
